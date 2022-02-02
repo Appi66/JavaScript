8bc28c98d9e9640be67d4d5ce61f18efdb6f7e4e
中村明日香 <96176617+Appi66@users.noreply.github.com>
2022-02-02 13:37:15 +0900

    [Finish]JS-3

--- app.js
+++ app.js
@@ -29,11 +29,82 @@
 
 // alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-let i;
-let num = 0;
+// let i;
+// let num = 0;
 
-for(i = 1; i < 11; i++){
-  num = num + i;
+// for(i = 1; i < 11; i++){
+  // num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+// let alertString;
+// alertString = addString("WebCamp");
+// 
+// alert(alertString);
+
+
+// function addString(strA){
+  // let addStr = "Hello" + strA;
+  // return addStr;
+// }
+
+
+// let promptStr = prompt('何か好きな文字を入力してください');
+
+// alert(promptStr)
+
+let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+
+let js_hand = getJShand();
+
+let judge = winLose(user_hand, js_hand);
+
+alert('あなたが選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3);
+  let hand_name;
+
+  if(js_hand_num == 0){
+    hand_name = "グー";
+  } else if(js_hand_num == 1){
+    hand_name = "チョキ";
+  } else if(js_hand_num == 2){
+    hand_name = "パー";
+  }
+
+  return hand_name;
 }
 
-alert('1から10まで足し算した結果は' + num + 'です');
+function winLose(user, js){
+  let winLoseStr;
+
+  if(user == "グー"){
+    if(js == "グー"){
+      winLoseStr = "あいこ";
+    } else if(js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if(js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if(user == "チョキ"){
+    if(js == "グー"){
+      winLoseStr = "負け";
+    } else if(js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if(js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if(user == "パー"){
+    if(js == "グー"){
+      winLoseStr = "勝ち";
+    } else if(js == "チョキ"){
+      winLoseStr = "負け";
+    } else if(js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+
+  return winLoseStr;
+}
